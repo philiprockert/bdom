9ce59ef81e3671255d88d068c9bb93a12838f61a
unknown <philiprockert@gmail.com>
2023-09-07 15:46:42 -0500

    v2

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,17 @@
   \**********************/
 /***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
-eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nvar express = __webpack_require__(/*! express */ \"express\");\nvar mysql = __webpack_require__(/*! mysql */ \"mysql\");\nvar app = express();\nvar db = mysql.createConnection({\n    host: 'localhost:3306',\n    user: 'YES_BPO-BI',\n    password: '',\n    database: 'bdom'\n});\ndb.connect(function (err) {\n    if (err) {\n        console.error('Error al conectar a la base de datos:', err);\n    }\n    else {\n        console.log('Conexión a la base de datos MySQL establecida');\n    }\n});\nvar port = process.env.PORT || 3001;\napp.listen(port, function () {\n    console.log(\"Servidor Express escuchando en el puerto \".concat(port));\n});\nvar initialState = {\n    cart: [],\n    products: [\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': '1234',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': 'Camiseta',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': 'Camiseta',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': 'Camiseta',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': 'Camiseta',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n        {\n            'id': '1',\n            'user': 'Felipe Rodriguez',\n            'password': 'Camiseta',\n            'telefono': 3167553547,\n            'email': 'philiprockert@gmail.com',\n        },\n    ],\n};\n\n\n//# sourceURL=webpack://pruebaexpress/./src/index.ts?");
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nvar express = __webpack_require__(/*! express */ \"express\");\nvar mysql = __webpack_require__(/*! mysql */ \"mysql\");\nvar app = express();\nvar router = express.Router();\nvar cors = __webpack_require__(/*! cors */ \"cors\");\n//creando conexion mysql\nvar db = mysql.createConnection({\n    host: 'localhost:3306',\n    user: 'YES_BPO-BI',\n    password: '',\n    database: 'bdom'\n});\n//establecer conexion\ndb.connect(function (err) {\n    if (err) {\n        console.error('Error al conectar a la base de datos:', err);\n    }\n    else {\n        console.log('Conexión a la base de datos MySQL establecida');\n    }\n});\nvar port = process.env.PORT || 3001;\n//consular datos\nvar query = 'SELECT * FROM usuarios';\ndb.query(query, function (err, results) {\n    if (err) {\n        console.error('error al obtener los datos:', err);\n        return;\n    }\n    console.log(results);\n    app.get('/', function (req, res) {\n        res.json(results[0].nombre);\n    });\n});\napp.listen(port);\n\n\n//# sourceURL=webpack://pruebaexpress/./src/index.ts?");
+
+/***/ }),
+
+/***/ "cors":
+/*!***********************!*\
+  !*** external "cors" ***!
+  \***********************/
+/***/ ((module) => {
+
+module.exports = require("cors");
 
 /***/ }),
 
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 const mysql = require('mysql');
 const app = express();
+const router = express.Router();
+const cors = require('cors');
+//creando conexion mysql
 const db = mysql.createConnection({
   host: 'localhost:3306',
   user: 'YES_BPO-BI',
   password: '',
   database: 'bdom'
 });
-
+//establecer conexion
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
@@ -17,61 +20,20 @@ db.connect((err) => {
     console.log('Conexión a la base de datos MySQL establecida');
   }})
   const port = process.env.PORT || 3001;
-  app.listen(port, () => {
-    console.log(`Servidor Express escuchando en el puerto ${port}`);
-  });
+  //consular datos
+  const query ='SELECT * FROM usuarios'
+  db.query(query, (err, results) => {if (err){
+  console.error('error al obtener los datos:', err);
+  return;
+  }
 
-const initialState = {
-  cart: [],
-  products: [
-    {
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': '1234',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-    {
-      
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': 'Camiseta',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-    {
-      
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': 'Camiseta',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-    {
-      
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': 'Camiseta',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-    {
- 
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': 'Camiseta',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-    {
-     
-      'id': '1',
-      'user': 'Felipe Rodriguez',
-      'password': 'Camiseta',
-      'telefono': 3167553547,
-      'email': 'philiprockert@gmail.com',
-    },
-  ],
-};
-
+  console.log(results);
+  
+app.get('/', (req, res)=>{
+  
+  res.json(results[0].nombre);
+  
+})
+})
+app.listen(port);
 
