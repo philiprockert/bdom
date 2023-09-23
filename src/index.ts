1d1a98e84a2837ecfedea3ea32ba475c7ea56ced
philiprockert <philiprockert@gmail.com>
2023-09-23 10:58:46 -0500

    p

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,44 @@
 
 import * as express from 'express';
-import { Request, Response } from 'express';
 const mysql = require('mysql');
-const app = express();
-const router = express.Router();
 const cors = require('cors');
-//creando conexion mysql
+
+const app = express();
+const port = 3001;
+app.use(cors());
+
 const db = mysql.createConnection({
-  host: 'localhost:3306',
-  user: 'YES_BPO-BI',
-  password: '',
-  database: 'bdom'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Dimebagphilip1971',
+  database: 'omnicom'
 });
-//establecer conexion
+
+// Establecer conexión a la base de datos
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
   } else {
     console.log('Conexión a la base de datos MySQL establecida');
-  }})
-  const port = process.env.PORT || 3001;
-  //consular datos
-  const query ='SELECT * FROM usuarios'
-  db.query(query, (err, results) => {if (err){
-  console.error('error al obtener los datos:', err);
-  return;
   }
+});
 
-  console.log(results);
-  
-app.get('/', (req, res)=>{
-  
-  res.json(results[0].nombre);
-  
-})
-})
-app.listen(port);
+// Definir la ruta y la lógica de consulta
+app.get('/', (req, res) => {
+  const query = 'SELECT * FROM usuario';
 
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al obtener los datos:', err);
+      return res.status(500).json({ error: 'Error al obtener los datos' });
+    }
+
+    // Enviar los resultados como respuesta JSON
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
